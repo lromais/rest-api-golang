f903fc6e94519bcd6d879139220626233b7db4ae
Lincoln Romais <lincoln.romais@concrete.com.br>
2020-07-07 09:44:48 -0300

    update teste nao finalizado

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rest-api-golang
 Estudo de golang rest api
+
+# Como executar
+
+1. `cd cmd`
+2. `go run main.go`
+
+# Como consultar api
+
+## Consulta por nome
+* ` curl http://localhost:3000/api/demo/hello/[nome]`
+
+## Consulta por soma de valores
+* `curl http://localhost:3000/api/demo/sum/[numero]/[numero]`
---
+++ demoapi_test.go
@@ -0,0 +1,50 @@
+package demoapi_test
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestGetHello(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Ola, Maria")
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", greeting)
+	// Output: Hello, Maria
+}
+
+func TestGetSum(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "1+2")
+	}))
+	defer ts.Close()
+
+	res, err := http.Get(ts.URL)
+	if err != nil {
+		log.Fatal(err)
+	}
+	greeting, err := ioutil.ReadAll(res.Body)
+	res.Body.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Printf("%s", greeting)
+	// Output: 3
+}
